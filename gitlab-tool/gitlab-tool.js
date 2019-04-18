7e31bf66f03b185ed687b9766cc8c46cd4e4e252
Devendra Tewari <devendra.tewari@gmail.com>
2019-04-18 13:33:39 -0300

    list all projects and milestones

--- gitlab-tool/gitlab-tool.js
+++ gitlab-tool/gitlab-tool.js
@@ -5,3 +5,51 @@ $("#back").on("click", () => {
 $(document).ready(() => {
 
 })
+
+$("#query").on("click", () => {
+    $("#spinner").removeClass("d-none")
+    var GitLab = require('gitlab/dist/es5').default
+    var api = new GitLab({
+        url: $("#url").val(),
+        token: $("#token").val()
+    })
+    api.Projects.all()
+    .then((projects) => {
+        $("#projects tbody").empty()
+        projects.forEach(p => {
+            api.ProjectMilestones.all(p.id)
+            .then((milestones) => {
+                var row = $("#projects tbody").append($("<tr>"))
+                row.append($("<td>").text(p.id))
+                row.append($("<td>").text(p.name))
+                row.append($("<td>").text(p.namespace.full_path))
+                row.append($("<td>").text(p.http_url_to_repo))
+                row.append($("<td>").text(p.ssh_url_to_repo))
+                milestones.forEach(m => {
+                    var row = $("#milestones tbody").append($("<tr>"))
+                    row.append($("<td>").text(m.id))
+                    row.append($("<td>").text(m.title))
+                    row.append($("<td>").text(p.name))
+                    row.append($("<td>").text(m.state))
+                    row.append($("<td>").text(m.due_date))
+                    row.append($("<td>").text(m.start_date))
+                })
+            })
+        });
+        $("#spinner").addClass("d-none")
+    })
+})
+
+$("#viewProjects").on("change", (e) => {
+    if (e.target.checked) {
+        $("#projects").removeClass("d-none")
+        $("#milestones").addClass("d-none")    
+    }
+})
+
+$("#viewMilestones").on("change", (e) => {
+    if (e.target.checked) {
+        $("#projects").addClass("d-none")
+        $("#milestones").removeClass("d-none")
+    }
+})
