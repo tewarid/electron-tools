9d0d6cc875f9f7039b042883943aa8d84231d2ea
Devendra Tewari <devendra.tewari@gmail.com>
2019-04-24 15:36:53 -0300

    fix tslint issues

--- src/git-tool/git-tool.ts
+++ src/git-tool/git-tool.ts
@@ -1,133 +1,126 @@
-import * as fs from "fs"
-import * as path from "path"
-import * as spawn from "child_process"
+import * as spawn from "child_process";
+import * as fs from "fs";
+import * as path from "path";
 
 $("#back").on("click", () => {
-    window.history.back()
-})
+    window.history.back();
+});
 
 $(document).ready(() => {
-    scan(window.localStorage.getItem("git-tool.rootFolder"))
-    $("#commands").text(window.localStorage.getItem("git-tool.commands"))
-    $("#log").text(window.localStorage.getItem("git-tool.log"))
-})
+    scan(window.localStorage.getItem("git-tool.rootFolder"));
+    $("#commands").text(window.localStorage.getItem("git-tool.commands"));
+    $("#log").text(window.localStorage.getItem("git-tool.log"));
+});
 
 $("#rootFolder").on("change", (e) => {
-    if ((<HTMLInputElement>e.target).files[0] == null) {
-        return
+    if ((e.target as HTMLInputElement).files[0] == null) {
+        return;
     }
-    var folder = (<HTMLInputElement>e.target).files[0].path
-    window.localStorage.setItem("git-tool.rootFolder", folder)
-    scan(folder)
-})
+    const folder = (e.target as HTMLInputElement).files[0].path;
+    window.localStorage.setItem("git-tool.rootFolder", folder);
+    scan(folder);
+});
 
 function scan(folder: string) {
     if (folder == null) {
-        return
+        return;
     }
-    $("label[for='rootFolder']").text(folder)
-    $("#folders").empty()
+    $("label[for='rootFolder']").text(folder);
+    $("#folders").empty();
     find(folder, (value: string) => {
         $("#folders").append($("<option>")
         .text(value)
-        .attr("value", value))
-    })
+        .attr("value", value));
+    });
 }
 
-function find(folder: string, found: Function) {
+function find(folder: string, found: (folder: string) => void) {
     fs.access(path.join(folder, ".git"), fs.constants.F_OK, (err) => {
         if (!err) {
-            found(folder)
+            found(folder);
         } else {
-            fs.readdir(folder, {withFileTypes: true}, (err: any, files) => {
+            fs.readdir(folder, {withFileTypes: true}, (dirErr: any, files) => {
                 files.forEach((f: any) => {
                     if (f.isDirectory()) {
-                        find(path.join(folder, f.name), found)
+                        find(path.join(folder, f.name), found);
                     }
-                })
-            })        
+                });
+            });
         }
-    })
+    });
 }
 
 $("#commands").on("input", (e) => {
-    window.localStorage.setItem("git-tool.commands", (<HTMLInputElement>e.target).value)
-})
+    window.localStorage.setItem("git-tool.commands", (e.target as HTMLInputElement).value);
+});
 
 function getSelectedCommands() {
-    var input = <HTMLInputElement>$("#commands")[0]
-    var commands = input.value
+    const input = $("#commands")[0] as HTMLInputElement;
+    let commands = input.value;
     if (input.selectionStart !==  input.selectionEnd) {
-        commands = commands.slice(input.selectionStart, input.selectionEnd)
+        commands = commands.slice(input.selectionStart, input.selectionEnd);
     }
-    return commands.split("\n")
-}
-
-class Command {
-    command: string;
-    folder: string;
-}
-
-class Config {
-    commands: Command[] = new Array()
-    executed(folder: string, command: string, output: string) {
-        $("#log").prepend(`$ ${command}\n${output}\n`)
-        $("#log").prepend(`${(new Date()).toISOString()} ${folder}\n`)
-    }
-    completed() {
-        $("#spinner").addClass("d-none")
-        window.localStorage.setItem("git-tool.log", $("#log").text())
-    }
+    return commands.split("\n");
 }
 
 $("#run").on("click", (e) => {
-    $("#spinner").removeClass("d-none")
-    var config = new Config()
+    $("#spinner").removeClass("d-none");
+    const config = {
+        commands: new Array(),
+        completed: () => {
+            $("#spinner").addClass("d-none");
+            window.localStorage.setItem("git-tool.log", $("#log").text());
+        },
+        executed: (folder: string, command: string, output: string) => {
+            $("#log").prepend(`$ ${command}\n${output}\n`);
+            $("#log").prepend(`${(new Date()).toISOString()} ${folder}\n`);
+        },
+    };
     $("#folders option:selected").each((index, option) => {
         getSelectedCommands().forEach((c: string) => {
             if (c.trim() === "") {
-                return
+                return;
             }
             config.commands.push({
                 command: "git " + c,
-                folder: (<HTMLInputElement>option).value
-            })
-        })
-    })
-    config.commands.reverse()
-    process(config)
-})
+                folder: (option as HTMLInputElement).value,
+            });
+        });
+    });
+    config.commands.reverse();
+    process(config);
+});
 
-function process(config: Config) {
-    if (config.commands.length == 0) {
-        config.completed()
-        return
+function process(config: any) {
+    if (config.commands.length === 0) {
+        config.completed();
+        return;
     }
-    var command = config.commands.pop()
+    const command = config.commands.pop();
     execute(command.folder, command.command)
     .then((value: string) => {
-        config.executed(command.folder, command.command, value)
-        process(config)
-    })
+        config.executed(command.folder, command.command, value);
+        process(config);
+    });
 }
 
 function execute(folder: string, command: string) {
-    return new Promise(function(resolve) {
-        var output: string = ""
-        var process = spawn.exec(command, {cwd: folder})
-        process.stdout.on("data", (data) => {
-            output += data
-        })
-        process.stderr.on("data", (data) => {
-            output += data            
-        })
-        process.on("close", () => {
-            resolve(output)
-        })
+    return new Promise((resolve) => {
+        let output: string = "";
+        const proc = spawn.exec(command, {cwd: folder});
+        proc.stdout.on("data", (data) => {
+            output += data;
+        });
+        proc.stderr.on("data", (data) => {
+            output += data;
+        });
+        proc.on("close", () => {
+            resolve(output);
+        });
     });
 }
 
 $("#clear").on("click", (e) => {
-    $("#log").text("")
-    window.localStorage.setItem("git-tool.log", "")
-})
+    $("#log").text("");
+    window.localStorage.setItem("git-tool.log", "");
+});
--- src/gitlab-tool/gitlab-tool.ts
+++ src/gitlab-tool/gitlab-tool.ts
@@ -1,71 +1,71 @@
-import {Gitlab} from "gitlab"
+import {Gitlab} from "gitlab";
 
 $("#back").on("click", () => {
-    window.history.back()
-})
+    window.history.back();
+});
 
 $(document).ready(() => {
-    var url = window.localStorage.getItem("gitlab-tool.url")
+    const url = window.localStorage.getItem("gitlab-tool.url");
     if (url) {
-        $("#host").val(url)
+        $("#host").val(url);
     }
-    var token = window.localStorage.getItem("gitlab-tool.token")
+    const token = window.localStorage.getItem("gitlab-tool.token");
     if (token) {
-        $("#token").val(token)
+        $("#token").val(token);
     }
-})
+});
 
 $("#host").on("input", (e) => {
-    window.localStorage.setItem("gitlab-tool.url", (<HTMLInputElement>e.target).value)
-})
+    window.localStorage.setItem("gitlab-tool.url", (e.target as HTMLInputElement).value);
+});
 
 $("#token").on("input", (e) => {
-    window.localStorage.setItem("gitlab-tool.token", (<HTMLInputElement>e.target).value)
-})
+    window.localStorage.setItem("gitlab-tool.token", (e.target as HTMLInputElement).value);
+});
 
 $("#query").on("click", () => {
-    $("#spinner").removeClass("d-none")
+    $("#spinner").removeClass("d-none");
     const api = new Gitlab({
         host: $("#host").val().toString(),
-        token: $("#token").val().toString()
-    })
+        token: $("#token").val().toString(),
+    });
     api.Projects.all()
     .then((projects: any) => {
-        $("#projects tbody").empty()
+        $("#projects tbody").empty();
         projects.forEach((p: any) => {
             api.ProjectMilestones.all(p.id, {})
             .then((milestones: any) => {
-                var row = $("#projects tbody").append($("<tr>"))
-                row.append($("<td>").text(p.id))
-                row.append($("<td>").text(p.name))
-                row.append($("<td>").text(p.namespace.full_path))
-                row.append($("<td>").text(p.http_url_to_repo))
-                row.append($("<td>").text(p.ssh_url_to_repo))
+                let row = $("#projects tbody").append($("<tr>"));
+                row.append($("<td>").text(p.id));
+                row.append($("<td>").text(p.name));
+                row.append($("<td>").text(p.namespace.full_path));
+                row.append($("<td>").text(p.http_url_to_repo));
+                row.append($("<td>").text(p.ssh_url_to_repo));
                 milestones.forEach((m: any) => {
-                    var row = $("#milestones tbody").append($("<tr>"))
-                    row.append($("<td>").text(m.id))
-                    row.append($("<td>").text(m.title))
-                    row.append($("<td>").text(p.name))
-                    row.append($("<td>").text(m.state))
-                    row.append($("<td>").text(m.due_date))
-                    row.append($("<td>").text(m.start_date))
-                })
-            })
+                    row = $("#milestones tbody").append($("<tr>"));
+                    row.append($("<td>").text(m.id));
+                    row.append($("<td>").text(m.title));
+                    row.append($("<td>").text(p.name));
+                    row.append($("<td>").text(m.state));
+                    row.append($("<td>").text(m.due_date));
+                    row.append($("<td>").text(m.start_date));
+                });
+            });
         });
-        $("#spinner").addClass("d-none")
-    })
-})
+        $("#spinner").addClass("d-none");
+    });
+});
 
 $("#viewProjects").on("change", (e) => {
-    if ((<HTMLInputElement>e.target).checked) {
-        $("#projects").removeClass("d-none")
-        $("#milestones").addClass("d-none")    
+    if ((e.target as HTMLInputElement).checked) {
+        $("#projects").removeClass("d-none");
+        $("#milestones").addClass("d-none");
     }
-})
+});
 
 $("#viewMilestones").on("change", (e) => {
-    if ((<HTMLInputElement>e.target).checked) {
-        $("#projects").addClass("d-none")
-        $("#milestones").removeClass("d-none")
+    if ((e.target as HTMLInputElement).checked) {
+        $("#projects").addClass("d-none");
+        $("#milestones").removeClass("d-none");
     }
-})
+});
