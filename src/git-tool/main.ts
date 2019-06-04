514101fc91aed40bcd27327f69718f145252f031
Devendra Tewari <devendra.tewari@gmail.com>
2019-06-04 09:29:41 -0300

    use knockout

--- src/git-tool/main.ts
+++ src/git-tool/main.ts
@@ -2,163 +2,165 @@ import * as spawn from "child_process";
 import CodeMirror from "codemirror";
 import "codemirror/mode/shell/shell";
 import * as fs from "fs";
+import * as ko from "knockout";
 import * as path from "path";
 import * as xterm from "xterm";
+import { ViewModelBase } from "../ko/common";
 
-const code = CodeMirror.fromTextArea(document.getElementById("commands") as HTMLTextAreaElement, {
-    lineNumbers: true,
-    mode: "shell",
-});
-code.setSize(null, "100");
-$(".CodeMirror").addClass("border border-primary rounded")
-    .css("font-size", "large");
-CodeMirror.on(code.getDoc(), "change", (instance, change) => {
-    window.localStorage.setItem("git-tool.commands", code.getValue());
-});
+class MainViewModel extends ViewModelBase {
+    private code: CodeMirror.EditorFromTextArea;
+    private term: xterm.Terminal;
+    private busy: ko.Observable;
+    private rootFolder: ko.Observable;
+    private gitFolders: ko.ObservableArray;
+    private selectedFolders: ko.ObservableArray;
 
-const term = new xterm.Terminal();
-term.setOption("convertEol", true);
-term.resize(80, 30);
-term.open(document.getElementById("log"));
-$(".xterm").addClass("border border-secondary rounded");
-
-$(document).ready(() => {
-    scan(window.localStorage.getItem("git-tool.rootFolder"));
-    code.setValue(window.localStorage.getItem("git-tool.commands") || "status");
-    term.write(window.localStorage.getItem("git-tool.log"));
-});
-
-let rootFolder: string;
-
-$("#rootFolder").on("change", (e) => {
-    if ((e.target as HTMLInputElement).files[0] == null) {
-        return;
+    constructor() {
+        ViewModelBase.prefix = "git-tool";
+        super();
+        this.typeName = "MainViewModel";
+        this.busy = ko.observable(false);
+        this.rootFolder = ko.observable("");
+        this.gitFolders = ko.observableArray();
+        this.selectedFolders = ko.observableArray();
+        this.setRootFolder(window.localStorage.getItem("git-tool.rootFolder"));
+        this.code = CodeMirror.fromTextArea(document.getElementById("commands") as HTMLTextAreaElement, {
+            lineNumbers: true,
+            mode: "shell",
+        });
+        this.code.setSize(null, "100");
+        $(".CodeMirror").addClass("border border-primary rounded").css("font-size", "large");
+        CodeMirror.on(this.code.getDoc(), "change", (instance, change) => {
+            window.localStorage.setItem("git-tool.commands", this.code.getValue());
+        });
+        this.code.setValue(window.localStorage.getItem("git-tool.commands") || "status");
+        this.term = new xterm.Terminal();
+        this.term.setOption("convertEol", true);
+        this.term.resize(80, 30);
+        this.term.open(document.getElementById("log"));
+        $(".xterm").addClass("border border-secondary rounded");
+        this.term.write(window.localStorage.getItem("git-tool.log"));
     }
-    rootFolder = (e.target as HTMLInputElement).files[0].path;
-    window.localStorage.setItem("git-tool.rootFolder", rootFolder);
-    scan(rootFolder);
-});
 
-function scan(root: string) {
-    if (root == null) {
-        return;
+    private setRootFolder(folder: string) {
+        this.rootFolder(folder);
+        window.localStorage.setItem("git-tool.rootFolder", folder);
+        this.scan(this.rootFolder());
     }
-    $("label[for='rootFolder']").text(root);
-    $("#folders").empty();
-    const selected = JSON.parse(window.localStorage.getItem("git-tool.selectedFolders")) as string[];
-    find(root, (value: string) => {
-        const option = $("<option>").text(value).attr("value", value);
-        if (selected && selected.indexOf(value) !== -1) {
-            option.attr("selected", "");
-        }
-        $("#folders").append(option);
-    });
-}
 
-function find(folder: string, found: (folder: string) => void) {
-    fs.access(path.join(folder, ".git"), fs.constants.F_OK, (err) => {
-        if (!err) {
-            found(folder);
-        } else {
-            fs.readdir(folder, {withFileTypes: true}, (dirErr: any, files) => {
-                files.forEach((f: any) => {
-                    if (f.isDirectory()) {
-                        find(path.join(folder, f.name), found);
-                    }
-                });
-            });
-        }
-    });
-}
-
-$("#folders").on("input", (e) => {
-    const selected = new Array();
-    $("#folders option:selected").each((index, option) => {
-        selected.push((option as HTMLInputElement).value);
-    });
-    window.localStorage.setItem("git-tool.selectedFolders", JSON.stringify(selected));
-});
-
-function getSelectedCommands() {
-    let commands = code.getDoc().getSelection();
-    if (commands === "") {
-        commands = code.getValue();
-    }
-    return commands.split("\n");
-}
-
-$("#run").on("click", (e) => {
-    $("#spinner").removeClass("d-none");
-    const config = {
-        commands: new Array(),
-        completed: () => {
-            $("#spinner").addClass("d-none");
-        },
-        executed: (result: any) => {
-            let s = `${(new Date()).toISOString()} ${result.command.folder}\n`;
-            s += `$ ${result.command.command}\n${result.output}\n`;
-            term.write(s);
-            window.localStorage.setItem("git-tool.log",
-                window.localStorage.getItem("git-tool.log") + s);
-        },
-    };
-    $("#folders option:selected").each((index, option) => {
-        config.commands = config.commands.concat(buildGitCommands((option as HTMLInputElement).value));
-    });
-    config.commands.reverse();
-    process(config);
-});
-
-function buildGitCommands(folder: string): any {
-    const commands: any = [];
-    getSelectedCommands().forEach((c: string) => {
-        if (c.trim() === "") {
+    private scan(root: string) {
+        if (root == null) {
             return;
         }
-        commands.push({
-            command: "git " + c,
-            folder,
+        this.gitFolders.removeAll();
+        this.find(root, (folder: string) => {
+            this.gitFolders.push(folder);
         });
-    });
-    return commands;
-}
-
-function process(config: any) {
-    if (config.commands.length === 0) {
-        config.completed();
-        return;
-    }
-    const command = config.commands.pop();
-    if (command.command.indexOf("git clone") !== -1) {
-        config.executed({command,
-            output: "\x1B[1;3;31mSkipping clone command. Please use submodule command instead.\x1B[0m\n"});
-        process(config);
-    } else {
-        execute(command.folder, command.command)
-        .then((value: string) => {
-            config.executed({command, output: value});
-            process(config);
+        this.selectedFolders.removeAll();
+        JSON.parse(window.localStorage.getItem("git-tool.selectedFolders"))
+        .forEach((value: string) => {
+            this.selectedFolders.push(value);
         });
     }
+
+    private find(folder: string, found: (folder: string) => void) {
+        try {
+            fs.accessSync(path.join(folder, ".git"), fs.constants.F_OK);
+            found(folder);
+        } catch {
+            const files = fs.readdirSync(folder, {withFileTypes: true});
+            files.forEach((f: any) => {
+                if (f.isDirectory()) {
+                    this.find(path.join(folder, f.name), found);
+                }
+            });
+        }
+    }
+
+    private runCommands() {
+        this.busy(true);
+        const config = {
+            commands: new Array(),
+            completed: () => {
+                this.busy(false);
+            },
+            executed: (result: any) => {
+                let s = `${(new Date()).toISOString()} ${result.command.folder}\n`;
+                s += `$ ${result.command.command}\n${result.output}\n`;
+                this.term.write(s);
+                window.localStorage.setItem("git-tool.log",
+                    window.localStorage.getItem("git-tool.log") + s);
+            },
+        };
+        this.selectedFolders().forEach((folder) => {
+            config.commands = config.commands
+                .concat(this.buildGitCommands(folder));
+        });
+        config.commands.reverse();
+        this.process(config);
+    }
+
+    private getSelectedCommands(): string[] {
+        let commands = this.code.getDoc().getSelection();
+        if (commands === "") {
+            commands = this.code.getValue();
+        }
+        return commands.split("\n");
+    }
+
+    private buildGitCommands(folder: string): any {
+        const commands: any = [];
+        this.getSelectedCommands().forEach((c: string) => {
+            if (c.trim() === "") {
+                return;
+            }
+            commands.push({
+                command: "git " + c,
+                folder,
+            });
+        });
+        return commands;
+    }
+
+    private process(config: any) {
+        if (config.commands.length === 0) {
+            config.completed();
+            return;
+        }
+        const command = config.commands.pop();
+        if (command.command.indexOf("git clone") !== -1) {
+            config.executed({command,
+                output: "\x1B[1;3;31mSkipping clone command. Please use submodule command instead.\x1B[0m\n"});
+            this.process(config);
+        } else {
+            this.execute(command.folder, command.command)
+            .then((value: string) => {
+                config.executed({command, output: value});
+                this.process(config);
+            });
+        }
+    }
+
+    private execute(folder: string, command: string) {
+        return new Promise((resolve) => {
+            let output: string = "";
+            const proc = spawn.exec(command, {cwd: folder});
+            proc.stdout.on("data", (data) => {
+                output += data;
+            });
+            proc.stderr.on("data", (data) => {
+                output += data;
+            });
+            proc.on("close", () => {
+                resolve(output);
+            });
+        });
+    }
+
+    private clearTerminal() {
+        this.term.clear();
+        window.localStorage.removeItem("git-tool.log");
+    }
 }
 
-function execute(folder: string, command: string) {
-    return new Promise((resolve) => {
-        let output: string = "";
-        const proc = spawn.exec(command, {cwd: folder});
-        proc.stdout.on("data", (data) => {
-            output += data;
-        });
-        proc.stderr.on("data", (data) => {
-            output += data;
-        });
-        proc.on("close", () => {
-            resolve(output);
-        });
-    });
-}
-
-$("#clear").on("click", (e) => {
-    term.clear();
-    window.localStorage.setItem("git-tool.log", "");
-});
+ko.applyBindings(new MainViewModel());
